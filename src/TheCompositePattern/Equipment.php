a611b93b50b13d92631006f9c467876db718889c
Marcin Zaremba <marcin.zaremba@gmail.com>
2015-10-07 09:41:16 +0200

    The Composite Pattern

---
+++ src/TheCompositePattern/Equipment.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcinos
+ * Date: 07.10.15
+ * Time: 09:29
+ */
+
+namespace PHPDesignPatterns\src\TheCompositePattern;
+
+
+abstract class Equipment
+{
+    abstract function getName();
+    abstract function getPrice();
+
+}
